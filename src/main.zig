8462bd6a221d7c2e10ce3ee4aa36ac7a9d350457
FelipeSharkao <felipenasgomes@gmail.com>
2024-06-29 10:51:13 -0300

    feat: handle directions

--- src/main.zig
+++ src/main.zig
@@ -28,12 +28,25 @@ const Vec2 = struct {
 
 const Snake = struct {
     head: Vec2,
+    dir: Vec2,
     fn init(x: i32, y: i32) Snake {
         return Snake{ .head = Vec2.init(x, y), .dir = Vec2.init(1, 0) };
     }
     fn update(s: *Snake) void {
+        if (wasKeyPressed(w4.BUTTON_UP) and s.dir.y == 0) {
+            s.nextDir = Vec2.init(0, -1);
+        } else if (wasKeyPressed(w4.BUTTON_DOWN) and s.dir.y == 0) {
+            s.nextDir = Vec2.init(0, 1);
+        } else if (wasKeyPressed(w4.BUTTON_LEFT) and s.dir.x == 0) {
+            s.nextDir = Vec2.init(-1, 0);
+        } else if (wasKeyPressed(w4.BUTTON_RIGHT) and s.dir.x == 0) {
+            s.nextDir = Vec2.init(1, 0);
+        }
+
         s.head = s.head.add(s.dir.scale(SNAKE_SPEED));
+
         log(0, 0, "head {d:.0} {d:.0}", .{ s.head.x, s.head.y });
+        log(0, 8, "dir {d:.0} {d:.0}", .{ s.dir.x, s.dir.y });
     }
     fn render(s: Snake) void {
         setColors(2, 3, 0, 0);
@@ -43,12 +56,18 @@ const Snake = struct {
 
 var snake = Snake.init(16, 80);
 
+var previous_gamepad: u8 = 0;
+
 export fn start() void {}
 
 export fn update() void {
     snake.update();
     snake.render();
     previous_gamepad = w4.GAMEPAD1.*;
+}
+
+fn wasKeyPressed(mask: u8) bool {
+    return (w4.GAMEPAD1.* & mask) != 0 and (previous_gamepad & mask) != 0;
 }
 
 fn setColors(c1: u4, c2: u4, c3: u4, c4: u4) void {
